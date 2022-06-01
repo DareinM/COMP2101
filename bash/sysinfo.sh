38aca59f9c29edeac8ea8a9b7c5801d55f31a017
Darein Mashahadi <dmashahadi@gmail.com>
2022-06-01 16:52:18 -0400

    Script done for now, Darein

---
+++ bash/sysinfo.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#filename
+$ ./sysinfo.sh
+
+#Hostname of my machine
+Host Name:darein-virtual-machine
+#Operating System running on this machine
+Operating System name: Ubuntu 22.04 LTS
+
+# Version of the operating System
+Version:Linux 5.15.0-33-generic
+
+#IP Addresses (IPv4 and IPv6)
+IPv4 Address: 192.168.139.128
+IPv6 Address:fe80::93de:3c6b:1484:14ca
+
+#Status of Root filesystem:
+Root Filesystem Status:
+Filesystem     1K-blocks    Used Available Use% Mounted on
+/dev/sda3       19946096 8199640  10707916  44% /
+ 
+#ending for now. I'll be back soon.
