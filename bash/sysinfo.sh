eb5d0755a7615fdbc0dc33a832941fbfd3e9e6ef
Darein Mashahadi <dmashahadi@gmail.com>
2022-06-02 11:17:56 -0400

    Updated

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
-
 #filename
-$ ./sysinfo.sh
+echo "$ ./sysinfo.sh"
 
 #Hostname of my machine
-Host Name:darein-virtual-machine
+echo "Host Name:darein-virtual-machine"
 #Operating System running on this machine
-Operating System name: Ubuntu 22.04 LTS
+echo "Operating System name: Ubuntu 22.04 LTS"
 
 # Version of the operating System
-Version:Linux 5.15.0-33-generic
+echo "Version:Linux 5.15.0-33-generic"
 
 #IP Addresses (IPv4 and IPv6)
-IPv4 Address: 192.168.139.128
-IPv6 Address:fe80::93de:3c6b:1484:14ca
+echo "IPv4 Address: 192.168.139.128"
+echo "IPv6 Address:fe80::93de:3c6b:1484:14ca"
 
 #Status of Root filesystem:
-Root Filesystem Status:
-Filesystem     1K-blocks    Used Available Use% Mounted on
-/dev/sda3       19946096 8199640  10707916  44% /
+echo "Root Filesystem Status:"
+echo "Filesystem     1K-blocks    Used Available Use% Mounted on"
+echo "/dev/sda3       19946096 8199640  10707916  44% /"
  
 #ending for now. I'll be back soon.
