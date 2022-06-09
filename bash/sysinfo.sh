5b012b10f1331a7e3049b41ed09e807a71800316
Darein Mashahadi <dmashahadi@gmail.com>
2022-06-08 22:26:46 -0400

    For lab1 and lab2 as i didnt yet get the marks for lab1

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
 #filename
 echo "$ ./sysinfo.sh"
+echo
+#For Bash Lab1
 
 #Hostname of my machine
 echo Hostname:"$HOSTNAME"
@@ -23,5 +26,21 @@ hostname -I
 #Status of Root filesystem:
 echo Root Filesystem Status:
 df -h /dev/sda3
- 
+
+#For Bash Lab2
+echo
+#Report of myvm
+echo Report for "$HOSTNAME"
+#command for lines style
+awk '{print}'  lines.txt
+# My Fully Qualified Domain Name(FQDN)
+echo -n "FQDN: "; hostname --fqdn
+#My operating system and version
+echo -n "Operating System name and version:" ;grep PRETTY /etc/os-release 
+#My IP address
+echo -n "IP Address:"; hostname -I
+#my root filesystem available space
+echo -n "Root Filesystem Free Space:" ; df -h --output=avail /dev/sda3 | grep '[0-5]'
+#ending lines
+awk '{print}' lines.txt
 #ending for now. I'll be back soon.
