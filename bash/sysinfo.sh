91e8a07aae99d41b4c618c2cd0acc41b4053a5aa
Darein Mashahadi <dmashahadi@gmail.com>
2022-06-23 08:54:22 -0400

    updated as instructed

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -3,30 +3,6 @@
 #filename
 echo "$ ./sysinfo.sh"
 echo
-#For Bash Lab1
-
-#Hostname of my machine
-echo Hostname:"$HOSTNAME"
-
-#Domain name of the system:
-echo Domain name: 
-hostname -d
-#Operating System running on this machine
-echo Operating System name: 
-grep PRETTY /etc/os-release
-
-# Version of the operating System
-echo Version:
-uname -r
-
-#IP Addresses (IPv4 and IPv6)
-echo IPv4:
-hostname -I
-
-#Status of Root filesystem:
-echo Root Filesystem Status:
-df -h /dev/sda3
-
 #For Bash Lab2
 echo
 #Report of myvm
