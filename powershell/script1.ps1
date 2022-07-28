992f9cf828a9fc1c9eec8690981902030495577d
DareinM <105839592+DareinM@users.noreply.github.com>
2022-07-28 15:40:17 -0400

    Lab3
    done

---
+++ powershell/script1.ps1
@@ -0,0 +1,6 @@
+#Lab3
+#IP Configuration Report
+get-ciminstance win32_networkadapterconfiguration |
+where { $_.ipenabled -eq "true" } |
+Format-Table -AutoSize Description, Index,
+IPAddress, IPSubnet, DNSDomain, DNSServerSearchOrder
